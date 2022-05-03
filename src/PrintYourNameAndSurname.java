f6431c085c596c08d7607716d51fb1c7802777c0
DavideGiusti87 <99611360+DavideGiusti87@users.noreply.github.com>
2022-05-03 12:43:27 +0200

    Esercizio5: Input and Output - 03

---
+++ src/PrintYourNameAndSurname.java
@@ -0,0 +1,16 @@
+/*
+Input and Output - 03
+Exercise: input-and-output-3
+create a class called PrintYourNameAndSurname
+provide the class PrintYourNameAndSurname with a main method
+inside the main method write two lines of code that print on 2 different lines your name and your surname
+the expected output should be this:
+YourName
+YourSurname
+ */
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        System.out.println("Davide");
+        System.out.println("Giusti");
+    }
+}
